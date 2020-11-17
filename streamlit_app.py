57accdaa99de4aa41962f85fca3fa6ad13a4ece7
Matteo Monchiero <matteo@streamlit.io>
2020-11-17 17:39:49 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ apps_active_per_day = st.slider("Fraction of active apps per day", 0.0, 1.0, 0.2
 hours_active_per_day = st.slider("Hours an app is active out of the 8 work hours in a day", 0.0, 8.0, 1.0, 0.1)
 mem_idle = st.slider("Memory for a idle app", 0.0, 64.0, 0.2, 0.1)
 mem_active = st.slider("Memory for an active app", 0.0, 64.0, 2.0, 0.1)
+memory_requests = st.slider("Memory Requests for an app", 0.0, 64.0, 0.2, 0.1)
 
 st.markdown("### System Specs")
 n_nodes = st.slider("Total number of nodes", 0, 1000, 300)
@@ -32,13 +33,12 @@ n_iter = st.slider("Number of iterations", 0, 1000, 100)
 
 #### Model ####
 
-apps_per_node = n_apps // n_nodes
+apps_per_node = math.ceil(max_mem / mem_requests)
 n_active = int(round(n_apps * apps_active_per_day * hours_active_per_day / 8))
 memory = [0,]*n_nodes
 all_mem_app_idle = mem_idle + manager_memory_idle + per_node_system_memory / apps_per_node
 all_mem_app_active = mem_active + manager_memory_active + per_node_system_memory / apps_per_node
 apps_mems = [all_mem_app_idle,]*(n_apps-n_active) + [all_mem_app_active,]*n_active
-mem_requests = max_mem / apps_per_node
 avg_active_per_node = n_active / n_nodes
 avg_idle_per_node = (n_apps - n_active) / n_nodes
 min_cores_per_node = math.ceil(avg_idle_per_node * (0.05 + manager_cores_idle) + avg_active_per_node * (1.0 + manager_cores_active) + per_node_system_cores)
@@ -67,7 +67,7 @@ st.write("Number of active apps:", n_active)
 st.write("Avg number of active apps per node:", avg_active_per_node)
 st.write("Minimum number of cores per node (suggested):", min_cores_per_node)
 st.write("Total CPUs:", min_cores_per_node * n_nodes)
-st.write("Memory requests:", mem_requests)
+#st.write("Memory requests:", mem_requests)
 st.write("Packing rate (apps per node):", apps_per_node)
 st.markdown("## Executive Metrics")
 st.write("Probability that one or more apps are out of memory in the cluster:")
