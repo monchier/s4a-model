f4aeeb7722b4dd02d05de8fe55cf0f26a83bd758
Matteo Monchiero <matteo@streamlit.io>
2020-11-17 17:57:27 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,11 @@ max_mem = st.slider("Max memory for a node", 1, 64, 16, 1)
 per_node_system_cores = 1.0
 per_node_system_memory = 0.5
 
-manager_cores_active = 0.25
-manager_memory_active = 0.5
+manager_cores_active = st.slider("Manager container active cores", 0.0, 1.0, 0.2, 0.1)
+manager_memory_active = st.slider("Manager container active memory", 0.0, 1.0, 0.5, 0.1)
 
-manager_cores_idle = 0.05
-manager_memory_idle = 0.05
+manager_cores_idle = st.slider("Manager container idle cores", 0.0, 1.0, 0.05, 0.1)
+manager_memory_idle = st.slider("Manager container idle memory", 0.0, 1.0, 0.1, 0.1)
 
 st.markdown("### Simulation Parameters")
 n_iter = st.slider("Number of iterations", 0, 1000, 100)
